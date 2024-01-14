733379788439c372d0712a02f37cb70db940fd2d
kevster2017 <kevin.okane2017@gmail.com>
2024-01-14 20:18:00 +0000

    css and welcome blade updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <img src="/images/rave.jpg" class="img-fluid" alt="Dancing Image">
+<div class="container-fluid" id="welcome">
+    <div class="row">
+        <div class="col">
+            <h1 class="text-center pt-5" id="title">Welcome to The Rave Cave</h1>
+            <h2 class="mt-5">The site that allows you to enjoy an online festival from the comfort of your home</h2>
+            <a href="#" class="btn btn-lg btn-success">Join us</a>
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+
 
 </div>
 

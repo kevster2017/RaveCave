760f5c81f55aedd933609004c5bee1621203ddf9
kevster2017 <kevin.okane2017@gmail.com>
2024-01-14 20:27:26 +0000

    style and welcome blade updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,11 @@
 <div class="container-fluid" id="welcome">
     <div class="row">
         <div class="col">
-            <h1 class="text-center pt-5" id="title">Welcome to The Rave Cave</h1>
-            <h2 class="mt-5">The site that allows you to enjoy an online festival from the comfort of your home</h2>
-            <a href="#" class="btn btn-lg btn-success">Join us</a>
+            <div class="welcomeText">
+                <h1 class="mt-5">Welcome to The Rave Cave</h1>
+                <h2 class="mt-5">The site that allows you to enjoy an online festival from the comfort of your home</h2>
+                <a href="{{ route('register') }}" class="btn btn-lg btn-success mt-5">Join us</a>
+            </div>
         </div>
         <div class="col">
 

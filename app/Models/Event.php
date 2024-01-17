56c27e636dbdea70f65f63caea6d56d5a09e7f44
kevster2017 <kevin.okane2017@gmail.com>
2024-01-17 09:52:29 +0000

    events model updated

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'date',
+        'time',
+        'dj',
+        'video',
+        'decsription',
+        'image'
+    ];
 }

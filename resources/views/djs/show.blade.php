73e516066c508d6846ab288fa259f955afcf45ee
kevster2017 <kevin.okane2017@gmail.com>
2024-01-19 20:18:54 +0000

    show blade updated

--- resources/views/djs/show.blade.php
+++ resources/views/djs/show.blade.php
@@ -22,11 +22,7 @@
             <div class="row g-0">
                 <div class="col-3">
                     <img src="/storage/{{$dj->image}}" class="img-responsive rounded-start m-3 img-fluid card-img" alt="dj Image">
-                    <div class="text-center">
-                        <a class="btn btn-primary mt-5" href="#">Add to favourites</a><br>
-                        <a class="btn btn-primary mt-5" href="#">Button 2</a><br>
-                        <a class="btn btn-primary mt-5" href="#">Button 3</a>
-                    </div>
+
 
                 </div>
 
@@ -36,10 +32,11 @@
 
                         <h2 class="card-title mb-3">{{ $dj->djname}}</h2>
 
-                        <p class="card-text">Home Town: {{ $dj->town}}</p>
-                        <p class="card-text">Country: {{ $dj->country }}</p>
-                        <p class="card-text">Genre: {{ $dj->genre }}</p>
-                        <p class="card-text"><small class="text-muted">Joined The Rave Cave: {{ $dj->created_at->diffForHumans() }}</small></p>
+                        <p class="card-text"><strong>Home Town:</strong> {{ $dj->town}}</p>
+                        <p class="card-text"><strong>Country:</strong> {{ $dj->country }}</p>
+                        <p class="card-text"><strong>Genre:</strong> {{ $dj->genre }}</p>
+                        <p class="card-text"><small class="text-muted"><strong>Joined The Rave Cave:</strong> {{ $dj->created_at->diffForHumans() }}</small></p>
+
 
                     </div>
                 </div>
@@ -49,13 +46,40 @@
 
         </div>
     </div>
+    <div class="row mb-5">
+        <div class="col-4">
+            <div class="card text-bg-light">
+                <h5 class="card-header">Follow {{ $dj->djname }}</h5>
+                <div class="card-body">
+                    <div class="row text-center ms-3">
+                        <div class="col">
+                            <a class="btn btn-primary" href="#">Add to favourites</a>
+                        </div>
+                        <div class="col">
+                            <a class="btn btn-primary" href="#">Button 2</a>
+                        </div>
+                        <div class="col">
+                            <a class="btn btn-primary" href="#">Button 3</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-bg-light">
+                <h5 class="card-header">About {{ $dj->djname }}</h5>
+                <div class="card-body">
 
-    <div class="card text-bg-light">
-        <h5 class="card-header">Description</h5>
-        <div class="card-body">
-            <p class="card-text"> {{ $dj->description }}</p>
+                    <div class="ms-5">
+                        <p class="card-text"><strong>Description:</strong> {{ $dj->town}}</p>
+                        <p class="card-text"><strong>Social Media:</strong> {{ $dj->country }}</p>
+                        <p class="card-text"><strong>Began DJ Career:</strong> {{ date('d/m/Y', strtotime($dj->date)) }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
 </div>
 
 

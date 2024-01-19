2762c8d0a47cd29a9a3998df7ab84f326fa5151b
kevster2017 <kevin.okane2017@gmail.com>
2024-01-19 20:40:49 +0000

    event show blade titles updated

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -20,7 +20,7 @@
     <div class="card text-bg-light mb-3">
         <div class="my-2">
             <div class="row g-0">
-                <div class="col-3">
+                <div class="col-4">
                     <img src="/storage/{{$event->image}}" class="img-responsive rounded-start m-3 img-fluid card-img" alt="event Image">
 
 
@@ -30,9 +30,9 @@
                     <div class="card-body">
 
 
-                        <h2 class="card-title mb-3">{{ $event->eventname}}</h2>
+                        <h2 class="card-title mb-3">{{ $event->title}}</h2>
 
-                        <p class="card-text"><strong>Home Town:</strong> {{ $event->town}}</p>
+                        <h3 class="card-text"> {{ $event->dj }}</h3>
                         <p class="card-text"><strong>Country:</strong> {{ $event->country }}</p>
                         <p class="card-text"><strong>Genre:</strong> {{ $event->genre }}</p>
                         <p class="card-text"><small class="text-muted"><strong>Joined The Rave Cave:</strong> {{ $event->created_at->diffForHumans() }}</small></p>
@@ -49,7 +49,7 @@
     <div class="row mb-5">
         <div class="col-12 col-md-4">
             <div class="card text-bg-light">
-                <h5 class="card-header">Follow {{ $event->eventname }}</h5>
+                <h5 class="card-header">Follow {{ $event->title }}</h5>
                 <div class="card-body">
                     <div class="row text-center ms-3">
                         <div class="col-12 col-md-4 mb-3 mb-md-0">
@@ -68,7 +68,7 @@
 
         <div class="col">
             <div class="card text-bg-light">
-                <h5 class="card-header">About {{ $event->eventname }}</h5>
+                <h5 class="card-header">About {{ $event->title }}</h5>
                 <div class="card-body">
 
                     <div class="ms-5">

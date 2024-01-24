24b609b72f987be51d2776cb4b2845b3f04b98ad
kevster2017 <kevin.okane2017@gmail.com>
2024-01-24 20:51:28 +0000

    all users section added to home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mb-5">
     <div class="row justify-content-center">
         <div class="row row-cols-1 row-cols-md-3 g-4">
 
@@ -45,6 +45,19 @@
                     </div>
                 </div>
             </div>
+
+            @if(auth()->user()->isAdmin == 1)
+            <div class="col">
+                <div class="card custom-card h-100">
+                    <a href=" {{ route('users.index') }}"><img src="/images/allUsers.jpg" class="card-img-top img-fluid" alt="Ravers dancing"></a>
+                    <div class="card-body">
+                        <h5 class="card-title">View All Users</h5>
+                        <p class="card-text">View the list of registered users</p>
+                    </div>
+                </div>
+            </div>
+
+            @endif
 
 
         </div>

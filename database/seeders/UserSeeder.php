1fe32dfc1dd7660c9ad0a9457269f45ed2b8fe3b
kevster2017 <kevin.okane2017@gmail.com>
2024-01-24 20:25:42 +0000

    images filepath updated on user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,12 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create(['name' => 'Kev', 'image' => 'public/images/profileImage.jpg', 'email' => 'kev@gmail.com', 'town' => 'Glengormley', 'Country' => 'Ireland', 'username' => 'Kevster', 'password' => bcrypt('password'), 'isAdmin' => 1]);
+        User::create(['name' => 'Kev', 'image' => '/images/profileImage.jpg', 'email' => 'kev@gmail.com', 'town' => 'Glengormley', 'Country' => 'Ireland', 'username' => 'Kevster', 'password' => bcrypt('password'), 'isAdmin' => 1]);
 
-        User::create(['name' => 'Eric', 'image' => 'public/images/profileImage.jpg', 'email' => 'Eric@gmail.com', 'town' => 'Glengormley', 'Country' => 'Ireland', 'username' => 'Ericster', 'password' => bcrypt('password'), 'isAdmin' => 0]);
+        User::create(['name' => 'Eric', 'image' => '/images/profileImage.jpg', 'email' => 'Eric@gmail.com', 'town' => 'Glengormley', 'Country' => 'Ireland', 'username' => 'Ericster', 'password' => bcrypt('password'), 'isAdmin' => 0]);
 
-        User::create(['name' => 'Luna', 'image' => 'public/images/profileImage.jpg', 'email' => 'luna@gmail.com', 'town' => 'Glengormley', 'Country' => 'Ireland', 'username' => 'Lunster', 'password' => bcrypt('password'), 'isAdmin' => 1]);
+        User::create(['name' => 'Luna', 'image' => '/images/profileImage.jpg', 'email' => 'luna@gmail.com', 'town' => 'Glengormley', 'Country' => 'Ireland', 'username' => 'Lunster', 'password' => bcrypt('password'), 'isAdmin' => 1]);
 
-        User::create(['name' => 'Mel', 'image' => 'public/images/profileImage.jpg', 'email' => 'mel@gmail.com', 'town' => 'Glengormley', 'Country' => 'Ireland', 'username' => 'Melster', 'password' => bcrypt('password'), 'isAdmin' => 1]);
+        User::create(['name' => 'Mel', 'image' => '/images/profileImage.jpg', 'email' => 'mel@gmail.com', 'town' => 'Glengormley', 'Country' => 'Ireland', 'username' => 'Melster', 'password' => bcrypt('password'), 'isAdmin' => 1]);
     }
 }

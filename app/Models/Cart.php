1a09180877fe9bc43061043366eafb8824af2432
kevster2017 <kevin.okane2017@gmail.com>
2024-01-29 20:21:25 +0000

    Cart and Ticket Models created

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+    public $table = "cart";
+
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
 }

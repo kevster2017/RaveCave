41a4fb386e2d0f0806d39915a48b1d0a56c80dd1
kevster2017 <kevin.okane2017@gmail.com>
2024-01-29 20:19:51 +0000

    Stripe controller, blade updated. Cart Model added

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -4,7 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Stripe;
+
 class StripeController extends Controller
 {
-    //
+    public function stripe()
+    {
+        return view('stripe');
+    }
+
+    public function stripePost(Request $request)
+    {
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        Stripe\Charge::create([
+            "amount" => 100 * 100,
+            "currency" => "GBP",
+            "source" => $request->stripeToken,
+            "description" => "This payment is testing purpose of The Rave Cave"
+        ]);
+
+        return redirect('/home')->with('success', 'Payment Successful');
+    }
 }

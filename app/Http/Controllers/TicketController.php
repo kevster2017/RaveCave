3d151af9878d4d274e5cbc7cfe9dd4b4cc8794e8
kevster2017 <kevin.okane2017@gmail.com>
2024-01-30 19:51:13 +0000

    myTickets() added to ticket controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ticket;
+use App\Models\Cart;
+use App\Models\Event;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class TicketController extends Controller
 {
-    //
+    public function addToCart()
+    {
+    }
+
+
+
+    function myTickets()
+    {
+
+        /* Get auth users id, get booking */
+        $userId = auth()->user()->id;
+
+        $tickets = DB::table('tickets')
+            ->where('userId', $userId)
+            ->get();
+
+        return view('/tickets/myTickets', ['tickets' => $tickets]);
+    }
 }

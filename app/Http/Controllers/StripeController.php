c8f9b05301fbe887051c4f5ac8680612df20dcfb
kevster2017 <kevin.okane2017@gmail.com>
2024-01-30 16:54:12 +0000

    stripePost function updated on StripeController

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use Stripe;
+use App\Models\Cart;
+use App\Models\Ticket;
 
 class StripeController extends Controller
 {
@@ -15,14 +18,40 @@ class StripeController extends Controller
 
     public function stripePost(Request $request)
     {
+
+        $userId = auth()->user()->id;
+        $fullCart = Cart::where('userID', $userId)
+            ->get();
+
+        foreach ($fullCart as $cart) {
+            $ticket = Ticket::where('userId', $userId)->get();
+
+            $ticket->userId = $cart->userId;
+            $ticket->paymentStatus = "Paid";
+        }
+
+        $total = DB::table('cart')
+            ->join('tickets', 'cart.ticket_id', '=', 'tickets.id')
+            ->where('cart.userId', $userId)
+            ->sum('tickets.price');
+
+        $total = ($total * 100);
+
         Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
         Stripe\Charge::create([
-            "amount" => 100 * 100,
+            "amount" => $total,
             "currency" => "GBP",
             "source" => $request->stripeToken,
             "description" => "This payment is testing purpose of The Rave Cave"
         ]);
 
-        return redirect('/home')->with('success', 'Payment Successful');
+        Cart::where('userId', $userId)->delete();
+
+        return redirect('/tickets/myTickets')->with('success', 'Payment Successful');
+    }
+
+
+    function orderTicket(Request $req)
+    {
     }
 }

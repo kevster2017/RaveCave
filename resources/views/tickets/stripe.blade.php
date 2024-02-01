8462e7b7a479ee3e92b5b719ac927412eddb979f
kevster2017 <kevin.okane2017@gmail.com>
2024-02-01 16:32:44 +0000

    form-floating added to stripe blade

--- resources/views/tickets/stripe.blade.php
+++ resources/views/tickets/stripe.blade.php
@@ -54,18 +54,18 @@
                         </div>
 
                         <div class='form-row row mt-3'>
-                           <div class='col-xs-12 col-md-4 form-group cvc required'>
-                              <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
-                              <label class='control-label'>CVC</label>
+                           <div class='form-floating col-xs-12 col-md-4 form-group cvc required'>
+                              <input autocomplete='off' class='form-control card-cvc' placeholder='CVC' size='4' type='text'>
+                              <label class='control-label ms-2'>CVC</label>
                            </div>
                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                              <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
-                              <label class='control-label'>Expiration Month</label>
+                              <input class='form-floating form-control card-expiry-month' placeholder='MM' size='2' type='text'>
+                              <label class='control-label ms-2'>Expiration Month</label>
 
                            </div>
-                           <div class='col-xs-12 col-md-4 form-group expiration required'>
+                           <div class='form-floating col-xs-12 col-md-4 form-group expiration required'>
                               <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
-                              <label class='control-label'>Expiration Year</label>
+                              <label class='control-label ms-2'>Expiration Year</label>
 
                            </div>
                         </div>

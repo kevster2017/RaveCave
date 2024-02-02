7691fff06947c604feb1beccc4032ec6124744e6
kevster2017 <kevin.okane2017@gmail.com>
2024-02-02 16:42:51 +0000

    myTickets link added to app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                         </li>
                         @endif
                         <li class="nav-item">
-                            <a class="nav-link" href="#">My Tickets</a>
+                            <a class="nav-link" href="{{ route('myTickets') }}">My Tickets</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('events.index') }}">Upcoming Events</a>

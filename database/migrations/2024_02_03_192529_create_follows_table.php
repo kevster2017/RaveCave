6bf519ac116e7340596364d1b5d4f61c47e56212
kevster2017 <kevin.okane2017@gmail.com>
2024-02-04 20:39:18 +0000

    columns added to favourites and follows table

--- database/migrations/2024_02_03_192529_create_follows_table.php
+++ database/migrations/2024_02_03_192529_create_follows_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('follows', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }

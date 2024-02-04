4c8018a88883a4f7fdd7c9fb8246be4b0dc95607
kevster2017 <kevin.okane2017@gmail.com>
2024-02-04 20:48:13 +0000

    Follow controller updated

--- app/Http/Controllers/FavouriteController.php
+++ app/Http/Controllers/FavouriteController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class FavouriteController extends Controller
 {
-    //
+    public function addToFavorites(Item $item)
+    {
+        auth()->user()->favorites()->syncWithoutDetaching([$item->id]);
+        return redirect()->back()->with('success', 'Item added to favorites.');
+    }
+
+    public function removeFromFavorites(Item $item)
+    {
+        auth()->user()->favorites()->detach($item->id);
+        return redirect()->back()->with('success', 'Item removed from favorites.');
+    }
 }
--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\User;
+
 use Illuminate\Http\Request;
 
 class FollowController extends Controller
 {
-    //
+    public function addTofollows(Event $id)
+    {
+        auth()->user()->follows()->syncWithoutDetaching([$id->id]);
+        return redirect()->back()->with('success', 'Item added to follows.');
+    }
+
+    public function removeFromfollows(Event $id)
+    {
+        auth()->user()->follows()->detach($id->id);
+        return redirect()->back()->with('success', 'Item removed from follows.');
+    }
 }

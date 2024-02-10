d0825f10b690137e99d482d21aca93e7a1b2afab
kevster2017 <kevin.okane2017@gmail.com>
2024-02-10 20:49:55 +0000

    columns added to database

--- database/migrations/2024_02_10_201733_create_foods_table.php
+++ database/migrations/2024_02_10_201733_create_foods_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('foods', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('businessName');
+            $table->string('image');
+            $table->string('town');
+            $table->string('type');
+            $table->string('description');
+            $table->text('webLink');
             $table->timestamps();
         });
     }

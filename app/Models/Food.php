bffe7f3ec775bea3c0b5754a0f3406eb0b92707e
kevster2017 <kevin.okane2017@gmail.com>
2024-02-10 21:01:30 +0000

    Food model updated

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -9,6 +9,8 @@ class Food extends Model
 {
     use HasFactory;
 
+    protected $table = 'foods'; // Explicitly specify the table name as laravel looking for a table called food
+
     protected $fillable = [
         'businessName',
         'image',

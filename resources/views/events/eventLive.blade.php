860a8fba0a75ba26ae321d3cf4b48481e79174da
kevster2017 <kevin.okane2017@gmail.com>
2024-02-10 20:07:56 +0000

    stage image updated. eventLive blade updated

--- resources/views/events/eventLive.blade.php
+++ resources/views/events/eventLive.blade.php
@@ -6,8 +6,10 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/home">Home</a></li>
-            <li class="breadcrumb-item"><a href="#">All Events</a></li>
-            <li class="breadcrumb-item active" aria-current="page"></li>
+            <li class="breadcrumb-item"><a href="/events">All Events</a></li>
+
+            <li class="breadcrumb-item"><a href="/events/show/{{ $event->id }}">{{ $event->title }} Page</a></li>
+            <li class="breadcrumb-item active" aria-current="page">{{ $event->title }} Live</li>
         </ol>
     </nav>
 </div>
@@ -15,7 +17,7 @@
 <div class="container-fluid eventImages mt-3" id="event">
 
     <img src="/images/valleypark.jpg" alt="Valley Park" class="event-image">
-    <img src="/images/stage.jpg" alt="Stage" class="overlay-imageStage">
+    <img src="/images/stage.png" alt="Stage" class="overlay-imageStage">
     <video class="video-overlay" controls>
         <source src="/storage/{{ $event->video }}" type="video/mp4">
         Your browser does not support the video tag.

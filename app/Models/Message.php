89d394f8e26def19dd5e8204c5d396841075cde4
kevster2017 <kevin.okane2017@gmail.com>
2024-02-11 19:29:03 +0000

    Message model updated

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'message',
+
+    ];
+
+    // Define relationship with User Model
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Define relationship with Event Model
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
 }

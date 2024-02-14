2a65b46fcd790bdc3f3cc8bdb38ddb66d562cfea
kevster2017 <kevin.okane2017@gmail.com>
2024-02-14 19:24:36 +0000

    event relationship updated on DJ model

--- app/Models/DJ.php
+++ app/Models/DJ.php
@@ -29,6 +29,11 @@ class DJ extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
     public function favouritedBy()
     {
         return $this->belongsToMany(User::class, 'favourites', 'dj_id', 'user_id');

6f7333a3d38cdd6d549d10e031a08298a8bb4c6d
kevster2017 <kevin.okane2017@gmail.com>
2024-02-14 19:38:08 +0000

    store() added to redeemticket controller

--- app/Http/Controllers/RedeemTicketController.php
+++ app/Http/Controllers/RedeemTicketController.php
@@ -21,5 +21,16 @@ class RedeemTicketController extends Controller
 
     public function store(Request $request, RedeemTicket $ticket)
     {
+        dd($request);
+
+        $ticket->user_id = $request->user_id;
+        $ticket->event_id = $request->event_id;
+        $ticket->dj_id = $request->dj_id;
+        $ticket->ticket_id = $request->ticket_id;
+        $ticket->djName = $request->djName;
+        $ticket->eventname = $request->eventName;
+        $ticket->redeemed = 1;
+
+        $ticket->save();
     }
 }

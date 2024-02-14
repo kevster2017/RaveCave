b37b86d5332179b0af5fceb580d393c97847d792
kevster2017 <kevin.okane2017@gmail.com>
2024-02-14 19:29:41 +0000

    index() added to redeemed tickets controller

--- app/Http/Controllers/RedeemTicketController.php
+++ app/Http/Controllers/RedeemTicketController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\RedeemTicket;
 
 class RedeemTicketController extends Controller
 {
     //
+    public function index()
+    {
+        $tickets = RedeemTicket::where('id', '>', 0)
+            ->orderBy('created_at', 'DESC')
+            ->get();
+
+        return view('redeemTickets.index', [
+            'tickers' => $tickets
+        ]);
+    }
 }

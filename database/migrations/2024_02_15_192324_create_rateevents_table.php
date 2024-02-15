ab67d5c04e206562df62d4a254537acbad4e947f
kevster2017 <kevin.okane2017@gmail.com>
2024-02-15 19:29:53 +0000

    columns added to rateevents table

--- database/migrations/2024_02_15_192324_create_rateevents_table.php
+++ database/migrations/2024_02_15_192324_create_rateevents_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('rateevents', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->integer('stars');
+            $table->string('comment');
             $table->timestamps();
         });
     }

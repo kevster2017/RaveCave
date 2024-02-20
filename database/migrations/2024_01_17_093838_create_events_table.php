6b8aa61d46c07b408020b5a7c1d374480987fe49
kevster2017 <kevin.okane2017@gmail.com>
2024-02-20 16:01:20 +0000

    dj_id foreign id added to create_events_table

--- database/migrations/2024_01_17_093838_create_events_table.php
+++ database/migrations/2024_01_17_093838_create_events_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->integer('userID');
+            $table->foreignId('dj_id')->constrained()->onDelete('cascade');
             $table->string('dj');
             $table->string('image');
             $table->string('video');

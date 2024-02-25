458b68d261d9f7b97d3bcaab4e3c9bb6110037f6
kevster2017 <kevin.okane2017@gmail.com>
2024-02-25 19:50:14 +0000

    djId column added to create_tickets_table

--- database/migrations/2024_01_29_202343_create_tickets_table.php
+++ database/migrations/2024_01_29_202343_create_tickets_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('userId');
             $table->string('dj');
+            $table->integer('djId');
             $table->string('image');
             $table->string('title');
             $table->date('date');

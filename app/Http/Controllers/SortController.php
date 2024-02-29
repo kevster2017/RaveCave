a210674c7a1df3e5eee961a9c06dc51a739ff832
kevster2017 <kevin.okane2017@gmail.com>
2024-02-29 19:58:59 +0000

    sort functions added to sort controller

--- app/Http/Controllers/SortController.php
+++ app/Http/Controllers/SortController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class SortController extends Controller
 {
-    //
+    public function priceHighLow()
+    {
+    }
+
+    public function priceLowHigh()
+    {
+    }
+
+    public function AtoZ()
+    {
+    }
+
+    public function ZtoA()
+    {
+    }
+
+    public function newestOldest()
+    {
+    }
+
+    public function oldestNewest()
+    {
+    }
 }

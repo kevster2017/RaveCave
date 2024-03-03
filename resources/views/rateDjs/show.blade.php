daa5bdcb1722014556aab74f20e7a8e297b6633c
kevster2017 <kevin.okane2017@gmail.com>
2024-03-03 20:12:43 +0000

    rateDjs show blade updated

--- resources/views/rateDjs/show.blade.php
+++ resources/views/rateDjs/show.blade.php
@@ -11,8 +11,8 @@
             <li class="breadcrumb-item"><a href="#" onclick="history.back();">DJ Page</a></li>
             <li class="breadcrumb-item active" aria-current="page">All ratings</li>
             @else
-            <li class="breadcrumb-item"><a href="{{ route('djs.show', $dj_id) }}">{{ $ratings->djName }}</a></li>
-            <li class="breadcrumb-item active" aria-current="page">{{ $ratings->djName }} ratings</li>
+            <li class="breadcrumb-item"><a href="#" onclick="history.back();">DJ Page</a></li>
+            <li class="breadcrumb-item active" aria-current="page">All ratings</li>
             @endif
 
         </ol>
